b9cfb47dd7e2db1de48b4654ea30379ad91c478e
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-14 18:17:11 -0400

    Moved current weather AJAX call into function

--- develop/script.js
+++ develop/script.js
@@ -20,40 +20,56 @@ $('#search-button').click(function (event) {
     // appaend the li element to the list of previously searched cities
     citiesSearchList.push($('#city-text').val());
     $('#city-list').append(city)
-    console.log(citiesSearch)
+    console.log(citiesSearchList)
+    renderCurrent();
 });
 
+// When a user searches for a city
+// I display the city name
+// The current temp
+// the current humidity
+// the current wind speed
+// the current UV index
+// need the lat
+// need the lon
+// The 5 day forecast
+// I need to save each city a user searches
+// When those saved cities are clicked again, the weather is displayed
+
 // Pull five day forecast
+function renderCurrent() {
+
+    // Pull data for current weather
+    $.ajax({
+        url: currentWeatherURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log("current city: ", response);
+        $("#current-city-info").empty()
+        // add searched city to page
+        $("#current-city-info").append($("<h2>").text(searchedCity));
+        // get current temp
+        var currentTemp = $("<p>").text("Current temperature: " + response.main.temp + "°F");
+        // add current temp to page
+        $("#current-city-info").append(currentTemp);
+        // get humidity
+        var currentHum = $("<p>").text("Current humidity: " + response.main.humidity + "%");
+        // add humidity to page
+        $("#current-city-info").append(currentHum);
+        // get wind speed
+        var windSpeed = $("<p>").text("Wind speed: " + response.wind.speed + "MPH");
+        // add wind speed to page
+        $("#current-city-info").append(windSpeed);
+        currentLon = response.coord.lon;
+        currentLat = response.coord.lat;
+
+    });
+
+}
 $.ajax({
     url: fiveDayForecastURL,
     method: "GET"
 }).then(function (response) {
     console.log("five day", response);
-});
-
-
-// Pull data for current weather
-$.ajax({
-    url: currentWeatherURL,
-    method: "GET"
-}).then(function (response) {
-    console.log("current city: ", response);
-    // add searched city to page
-    $("#current-city-info").append($("<h2>").text(searchedCity));
-    // get current temp
-    var currentTemp = $("<p>").text("Current temperature: " + response.main.temp + "°F");
-    // add current temp to page
-    $("#current-city-info").append(currentTemp);
-    // get humidity
-    var currentHum = $("<p>").text("Current humidity: " + response.main.humidity + "%");
-    // add humidity to page
-    $("#current-city-info").append(currentHum);
-    // get wind speed
-    var windSpeed = $("<p>").text("Wind speed: " + response.wind.speed + "MPH");
-    // add wind speed to page
-    $("#current-city-info").append(windSpeed);
-    currentLon = response.coord.lon;
-    currentLat = response.coord.lat;
-
 });
 
