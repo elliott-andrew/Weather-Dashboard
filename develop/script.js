c2dfb31bc1527862c61b03f94767db2218f891fe
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-10-13 20:05:43 -0400

    Added information for current weather

--- develop/script.js
+++ develop/script.js
@@ -1,15 +1,28 @@
 // This is our API key. Add your own API key between the ""
 var searchedCity = "Seattle";
 
-// Here we are building the URL we need to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=068008542218df571052276addfd8640";
+// URL for current weather data
+var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&units=imperial&appid=068008542218df571052276addfd8640";
 
-// We then created an AJAX call
+// Pull data for current weather
 $.ajax({
-    url: queryURL,
+    url: currentWeatherURL,
     method: "GET"
 }).then(function (response) {
-    console.log(queryURL);
     console.log(response);
-    console.log(response.main.temp);
+    // add searched city to page
+    $("#current-city-info").append($("<h2>").text(searchedCity))
+    // get current temp
+    var currentTemp = $("<p>").text("Current temperature: " + response.main.temp + "°F");
+    // add current temp to page
+    $("#current-city-info").append(currentTemp)
+    // get humidity
+    var currentHum = $("<p>").text("Current humidity: " + response.main.humidity + "%");
+    // add humidity to page
+    $("#current-city-info").append(currentHum);
+    // get wind speed
+    var windSpeed = $("<p>").text("Wind speed: " + response.wind.speed + "MPH");
+    // add wind speed to page
+    $("#current-city-info").append(windSpeed);
+
 });
